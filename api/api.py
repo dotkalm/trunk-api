ff1163035c6d2d6aa31784a45286e23c65763c86
dotkalm <joelmholmberg@gmail.com>
2019-09-06 12:24:47 -0700

    add big

--- api/api.py
+++ api/api.py
@@ -13,7 +13,7 @@ def save_picture(form_picture):
    random_hex = secrets.token_hex(8)
    f_name, f_ext = os.path.splitext(form_picture.filename)
    picture_name = random_hex + f_ext
-   file_path_for_avatar = os.path.join(os.getcwd(), 'static/items' + picture_name)
+   file_path_for_avatar = os.path.join(os.getcwd(), 'static/items/' + picture_name)
    output_size = (800, 800)
    i = Image.open(form_picture)
    i.thumbnail(output_size)
@@ -24,6 +24,15 @@ def save_picture(form_picture):
    print(width,'width',height,'height')
    i.save(file_path_for_avatar)
    return picture_name
+@api.route('/bins/', methods=["POST"])
+def create_bin():
+    print(request, '<--request')
+    payload = request.form.to_dict()
+    print(payload, '<-payload', type(payload), 'type')
+    item = models.Bin.create(**payload)
+    item_dict = model_to_dict(item)
+    print(item.__dict__)
+    return jsonify(data=item_dict, status={"code":201, "message":"success"})
 
 @api.route('/', methods=["POST"])
 def create_junk():
@@ -37,7 +46,7 @@ def create_junk():
     file_picture_path = save_picture(dict_file['file'])
     payload['image'] = file_picture_path
     print(payload, '<-payload', type(payload), 'type')
-    item = models.Junk.create(**payload)
+    item = models.Item.create(**payload)
     item_dict = model_to_dict(item)
     print(item.__dict__)
     return jsonify(data=item_dict, status={"code":201, "message":"success"})
@@ -45,7 +54,7 @@ def create_junk():
 @api.route('/', methods=["GET"])
 def get_all_items():
     try: 
-        items = [model_to_dict(item) for item in models.Junk.select()]
-        return jsonify(items)
+        items = [model_to_dict(item) for item in models.Item.select()]
+        return jsonify(data=items, status={"code": 200, "message":"success"})
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message": "seems to be a problem retrieving"})
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import datetime
 import os
 
 #DATABASE = connect(os.environ.get('DATABASE_URL'))
-DATABASE = SqliteDatabase('junk2.sqlite')
+DATABASE = SqliteDatabase('items.sqlite')
 
 #tags = db.Table('tags',
  #   db.Column('location_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
@@ -19,20 +19,24 @@ class User(UserMixin, Model):
    password = CharField()
    class Meta:
        database = DATABASE 
+class Bin(Model):
+    size = CharField()
+    class Meta:
+        database = DATABASE
 
-class Junk(Model):
+class Item(Model):
     #average_red = IntegerField()
     #average_green = IntegerField()
     #average_blue = IntegerField()
     image = CharField()
     description = TextField()
-    #author = ForeignKeyField(User, backref='junk')
+    bin = ForeignKeyField(Bin, backref='item')
     price = FloatField()
     class Meta:
        database = DATABASE 
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User, Junk], safe=True)
+    DATABASE.create_tables([User, Item, Bin], safe=True)
     print("your TABLES CREATED")
     DATABASE.close()
